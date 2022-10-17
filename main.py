362a9de824a2a6675715070b1713e8a20b23af22
sanjarbek <sanjarsaidov173@gmail.com>
2022-10-18 00:08:31 +0500

    progressive is good

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from pytube import YouTube
 
 yt = YouTube('https://www.youtube.com/watch?v=NjvwWiCYLl4&ab_channel=JonathanArrington')
 
-streams = yt.streams.asc()
+streams = yt.streams.filter(progressive=True)
 
 resolutions = []
 for stream in streams:
     resolutions.append(stream.resolution)
 print(resolutions)
+
